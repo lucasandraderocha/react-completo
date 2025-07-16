2c1c1ccd0e911d97cb5784b123f76ca6d684bfd1
Lucas de Andrade Rocha <lucas@ecombase.com.br>
2025-07-16 18:22:02 -0300

    useMemo: Iniciado os estudos sorbe o hook.

---
+++ 2.0 react-hooks/2.4 useMemo/src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from "react";
+
+import Lesson from "./lesson/Lesson";
+import Test from "./test/Test";
+
+function App() {
+  const [lesson, setLesson] = useState(true);
+  const handleShowClick = () => {
+    setLesson(!lesson);
+  };
+  return (
+    <>
+      <div>
+        <button onClick={handleShowClick}>
+          {lesson ? "Mostrar testes" : "Mostrar Aula"}
+        </button>
+      </div>
+      <br />
+      {!lesson && <Test />}
+      {lesson && <Lesson />}
+    </>
+  );
+}
+
+export default App;
---
+++ 2.0 react-hooks/2.4 useMemo/src/main.jsx
@@ -0,0 +1,9 @@
+import { createRoot } from "react-dom/client";
+import React from "react";
+import App from "./App.jsx";
+const root = document.getElementById("root");
+createRoot(root).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
