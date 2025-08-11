eced543d09992a6f70f7380c5cbb122a1f5220be
Lucas de Andrade Rocha <lucas@ecombase.com.br>
2025-08-11 16:07:44 -0300

    useReducer: Iniciado o processo para o projeto final.

--- 5.0 mais-react/5.4 useReducer/src/components/CounterReducer.jsx
+++ 5.0 mais-react/5.4 useReducer/src/components/CounterReducer.jsx
@@ -1,73 +1,110 @@
-import { useReducer } from "react";
+import { useReducer, useState } from "react";
 
 import Button from "../../../5.2 lazy-suspense/src/components/Button";
 import Container from "../../../5.2 lazy-suspense/src/components/Container";
 
 const initial = {
-  counter: 0,
-  role: {
-    level: 0,
-    degree: ["common", "admin", "super"],
-  },
+  count: 0,
+};
+
+const actions = {
+  increment: "increment",
+  decrement: "decrement",
+  multiply: "multiply",
+  divide: "divide",
+  square: "square",
+  custom: "custom",
 };
 
 const reducer = (state, action) => {
-  const operations = {
-    add: () => ({ ...state, counter: state.counter + 1 }),
+  const { increment, decrement, multiply, divide, square, custom } = actions;
 
-    subtract: () => ({ ...state, counter: state.counter - 1 }),
-
-    multiply: () => ({ ...state, counter: state.counter * 1 }),
-
-    divide: () => ({ ...state, counter: state.counter / 2 }),
-
-    square: () => ({ ...state, counter: state.counter * state.counter }),
-
-    degreeUp: () => ({
-      ...state,
-      role: { ...state.role, level: state.role.level + 1 },
-    }),
-  };
-
-  const operation = operations[action.type];
-  if (operation) {
-    return operation();
+  switch (action.type) {
+    case increment:
+      return { count: state.count + 1 };
+    case decrement:
+      return { count: state.count - 1 };
+    case multiply:
+      return { count: state.count * 2 };
+    case divide:
+      return { count: state.count / 2 };
+    case square:
+      return { count: state.count / state.count };
+    case custom:
+      return { count: action.payload };
+    default:
+      return state;
   }
-  return state;
 };
 
 const CounterReducer = () => {
   const [state, dispatch] = useReducer(reducer, initial);
+  const [customValue, setCustomValue] = useState(0);
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    console.log(customValue);
+    dispatch({ type: actions.custom, payload: customValue });
+  };
 
   return (
     <>
-      <Container className="flex gap-8">
-        <Container className="flex w-fit py-8 px-24 brd-sm rds-sm justify-center align-center">
-          <p>{state.counter}</p>
+      <Container className="flex flex-column gap-8">
+        <Container className="flex py-8 px-24 brd-sm rds-sm justify-center align-center">
+          <p>{state.count}</p>
         </Container>
         <Container className="flex flex-1 py-8 px-8 gap-8 w-full brd-sm rds-sm justify-center">
           <Button
+            onClick={() => dispatch({ type: actions.divide })}
             className="py-8 px-24 color-neutral-deep rds-sm pointer"
-            onClick={() => dispatch({ type: "subtract" })}
           >
-            ⇣
+            /
           </Button>
           <Button
+            onClick={() => dispatch({ type: actions.decrement })}
             className="py-8 px-24 color-neutral-deep rds-sm pointer"
-            onClick={() => dispatch({ type: "add" })}
           >
-            ⇡
+            -
+          </Button>
+          <Button
+            onClick={() => dispatch({ type: actions.increment })}
+            className="py-8 px-24 color-neutral-deep rds-sm pointer"
+          >
+            +
+          </Button>
+          <Button
+            onClick={() => dispatch({ type: actions.multiply })}
+            className="py-8 px-24 color-neutral-deep rds-sm pointer"
+          >
+            ×
+          </Button>
+          <Button
+            onClick={() => dispatch({ type: actions.multiply })}
+            className="py-8 px-24 color-neutral-deep rds-sm pointer"
+          >
+            √
           </Button>
         </Container>
-      </Container>
-      <Container className="flex flex-column py-8 px-8 gap-8 w-full brd-sm rds-sm align-center justify-center">
-        <p>{state.role.degree[state.role.level]}</p>
-        <Button
-          onClick={() => dispatch({ type: "degreeUp" })}
-          className="py-8 px-24 w-fit color-neutral-deep rds-sm pointer"
-        >
-          Level up ⇡⇡
-        </Button>
+        <Container className="flex flex-column flex-1 py-8 px-8 gap-8 w-full brd-sm rds-sm justify-center">
+          <form onSubmit={handleSubmit} className="flex flex-column gap-8">
+            <label className="flex flex-column gap-4 pointer">
+              Valor customizado
+              <input
+                type="number"
+                value={customValue}
+                onChange={e => setCustomValue(e.target.value)}
+                placeholder="insira um valor customizado"
+                className="py-8 px-12 brd-sm rds-sm w-full typo-main"
+              />
+            </label>
+            <Button
+              type="submit"
+              className="py-8 px-24 color-neutral-deep rds-sm pointer"
+            >
+              Customizar Valor
+            </Button>
+          </form>
+        </Container>
       </Container>
     </>
   );
